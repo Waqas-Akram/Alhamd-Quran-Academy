810c6e679af911085dcb111bb4fd57ab2c6b579a
Waqas Akram <waqasassasin@gmail.com>
2021-03-23 20:55:06 +0500

    Addmission form is being sented on email

---
+++ backend/routes/api/contact.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const router = express.Router();
+
+//Post email message with the help of nodemailer transport
+
+router.post("/send", async (req, res) => {
+  let {
+    studentName,
+    fatherName,
+    motherName,
+    birthDate,
+    age,
+    course,
+    detail,
+    email,
+    phoneNumber,
+  } = req.body;
+  age = age + " " + "years";
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Outlook",
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+    const mailOptions = {
+      from: process.env.EMAIL_ADDRESS,
+      to: process.env.RECIEVER_EMAIL_ADDRESS,
+      subject: "New Admission Recieved from " + studentName,
+      text: "",
+      html:
+        "<p><ul><li>Student Name :" +
+        studentName +
+        "</li><li>Father Name :" +
+        fatherName +
+        "</li><li>Mother Name :" +
+        motherName +
+        "</li><li>Birthdate :" +
+        birthDate +
+        "</li><li>Father's Email : " +
+        email +
+        "</li><li>Phone Number : " +
+        phoneNumber +
+        "</li><li>Total Age of Student :" +
+        age +
+        "</li><li>Course Applied:" +
+        course +
+        "</li><li>Extra detail : " +
+        detail +
+        "</li></ul> </p>",
+    };
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      }
+      res.status(200).json({ msg: "Message Sent Successfully" });
+    });
+  } catch (error) {
+    console.error(erro.message);
+    res.status(500).json({ msg: "Message Error" });
+  }
+});
+
+module.exports = router;
