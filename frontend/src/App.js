b4e39e4cd7d6c02e1828cd6913a380708394dc44
Waqas Akram <waqasassasin@gmail.com>
2021-03-24 19:21:57 +0500

    Spinner Animation Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavHeader from "./components/layout/NavHeader";
 import Routing from "./routes/Routing";
+import SpinnerAnimation from "./components/animations/spinner/SpinnerAnimation";
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    window.load = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
+  return loading ? (
+    <SpinnerAnimation />
+  ) : (
     <Router>
       <Fragment>
         <NavHeader />
