71bf9fc10538de47423ee5184e9e449325104031
Waqas Akram <waqasassasin@gmail.com>
2021-03-28 01:08:50 +0500

    Padding decreased in detail section

--- frontend/src/components/admin/SingleStudent.jsx
+++ frontend/src/components/admin/SingleStudent.jsx
@@ -1,11 +1,5 @@
 import React, { Fragment, useState, useEffect, useRef } from "react";
-import {
-  Row,
-  Col,
-  Container,
-  Jumbotron,
-  Button,
-} from "react-bootstrap";
+import { Row, Col, Container, Jumbotron, Button } from "react-bootstrap";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import ReactToPrint from "react-to-print";
@@ -44,101 +38,95 @@ const SingleStudent = ({ match }) => {
 
           <Jumbotron className="my-4 bg-white text-dark p-1" ref={componentRef}>
             <Row>
-              <Col xs='12' md='6' xl='6' sm='6'>
+              <Col xs="12" md="6" xl="6" sm="6">
                 <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Name</b> : &nbsp;{student.studentName}
                   </h3>
                 </div>
-                </Col>
-              <Col xs='12' md='6' xl='6' sm='6'>
-              <div style={{ backgroundColor: "#F7F7F9" }}>
+              </Col>
+              <Col xs="12" md="6" xl="6" sm="6">
+                <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Father's Name</b> : &nbsp;{student.fatherName}
                   </h3>
                 </div>
               </Col>
-              </Row>
-              <Row>
-                <Col xs='12' md='6' xl='6' sm='6'>
+            </Row>
+            <Row>
+              <Col xs="12" md="6" xl="6" sm="6">
                 <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Mother's Name</b> : &nbsp;{student.motherName}
                   </h3>
                 </div>
-                </Col>
-                <Col xs='12' md='6' xl='6' sm='6'>
+              </Col>
+              <Col xs="12" md="6" xl="6" sm="6">
                 <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Birth Date</b> : &nbsp;{student.birthDate}
                   </h3>
                 </div>
-                </Col>
-
-              </Row>
-              <Row>
-              <Col xs='12' md='6' xl='6' sm='6'>
-              <div style={{ backgroundColor: "#F7F7F9" }}>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="12" md="6" xl="6" sm="6">
+                <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Email</b> : &nbsp;{student.email}
                   </h3>
                 </div>
-                </Col><Col xs='12' md='6' xl='6' sm='6'>
+              </Col>
+              <Col xs="12" md="6" xl="6" sm="6">
                 <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Phone Number </b> : &nbsp;{student.phoneNumber}
                   </h3>
                 </div>
-                </Col>
-              </Row>
-              <Row>
-              <Col xs='12' md='6' xl='6' sm='6'>
-              <div style={{ backgroundColor: "#F7F7F9" }}>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="12" md="6" xl="6" sm="6">
+                <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Applied In</b> : &nbsp;{student.course}
                   </h3>
                 </div>
-                </Col><Col xs='12' md='6' xl='6' sm='6'>
+              </Col>
+              <Col xs="12" md="6" xl="6" sm="6">
                 <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Selected Language</b> : &nbsp;{student.language}
                   </h3>
                 </div>
-                </Col>
-              </Row>
-              <Row>
-              <Col xs='12' md='6' xl='6' sm='6'>
-              <div style={{ backgroundColor: "#F7F7F9" }}>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="12" md="6" xl="6" sm="6">
+                <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
                     <b>Address</b> : &nbsp;{student.address}
                   </h3>
                 </div>
-                </Col><Col xs='12' md='6' xl='6' sm='6'>
+              </Col>
+              <Col xs="12" md="6" xl="6" sm="6">
                 <div style={{ backgroundColor: "#F7F7F9" }}>
                   <h3 className="p-3">
-                    <b>Registered on</b> : &nbsp;<Moment format='DD MMMM YYYY'>{student.date}</Moment>
+                    <b>Registered on</b> : &nbsp;
+                    <Moment format="DD MMMM YYYY">{student.date}</Moment>
                   </h3>
                 </div>
-                </Col>
-              </Row>
-              <Row>
-              <Col >
-              <div style={{ backgroundColor: "#F7F7F9" }}>
-                  <h3 className="p-5">
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div style={{ backgroundColor: "#F7F7F9" }}>
+                  <h3 className="p-3">
                     <b>Extra Detail</b> : &nbsp;{student.detail}
                   </h3>
                 </div>
-                </Col>
-              </Row>
-                
-               
-                
-              
-              
-               
-              
-              
-           
+              </Col>
+            </Row>
           </Jumbotron>
         </Container>
       </div>
