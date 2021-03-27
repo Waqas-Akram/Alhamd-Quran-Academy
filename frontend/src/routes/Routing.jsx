ac193a2d9b7e677a364b03991961138066cbc714
Waqas Akram <waqasassasin@gmail.com>
2021-03-27 22:55:07 +0500

    Not found and getting back single studet

--- frontend/src/routes/Routing.jsx
+++ frontend/src/routes/Routing.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
-import Landing from '../components/layout/Landing'
-import AdmissionForm from '../components/forms/AdmissionForm'
-import Books from '../components/books/Books'
-import Contact from '../components/contact/Contact'
-import FeeSection from '../components/fee/FeeSection'
+import Landing from "../components/layout/Landing";
+import AdmissionForm from "../components/forms/AdmissionForm";
+import Books from "../components/books/Books";
+import FeeSection from "../components/fee/FeeSection";
+import Admin from "../components/admin/Admin";
+import NotFound from "../components/error/NotFound";
+import SingleStudent from "../components/admin/SingleStudent";
 
 const Routing = () => {
   return (
@@ -13,9 +15,10 @@ const Routing = () => {
         <Route exact path="/" component={Landing} />
         <Route exact path="/admission" component={AdmissionForm} />
         <Route exact path="/books" component={Books} />
-        <Route exact path="/contact" component={Contact} />
         <Route exact path="/fee" component={FeeSection} />
-        <Route exact path="*" ><h1>Error ! Not Found</h1></Route>
+        <Route exact path="/admin" component={Admin} />
+        <Route exact path="/admin/students/:id" component={SingleStudent} />
+        <Route exact path="*" component={NotFound} />
       </Switch>
     </Fragment>
   );
