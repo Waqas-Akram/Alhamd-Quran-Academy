62383ddaac51844c0afca777f7d3478e90538e83
Waqas Akram <waqasassasin@gmail.com>
2021-03-28 14:46:42 +0500

    Courses array

---
+++ frontend/src/components/layout/CourseArray.js
@@ -0,0 +1,31 @@
+const courseArray = [
+  {
+    id: 1,
+    name: "Madni Qaidah",
+    image: "/images/madni.png",
+    discription:
+      "This course is designed for those who are newbies. This course is a base or we can called it as a first step into Quran Learning Process. After completing this course students will be able to identify the Arabic words and will be able to pronounce them",
+  },
+  {
+    id: 2,
+    name: "Holy Quran Reading (Nazrah)",
+    image: "/images/Quran_course.png",
+    discription:
+      "This course is designed for those who want to learn how to read Quran Pak.In this course we will only learn Arabic reading of Quran",
+  },
+  {
+    id: 3,
+    name: "Memorizing The Holy Quran (Hifz)",
+    image: "/images/memorize.png",
+    discription:
+      "This course is designed for those who want to learn the Holy Quran by heart. The Holy Quran is the only book in the world that can be memorized by word to word. Children of every age can do that by giving full devotion",
+  },
+  {
+    id: 4,
+    name: "Learning Prayer (Namaz)",
+    image: "/images/namaz.png",
+    discription:
+      "This course is designed for those who wish to learn all basic to advanced features of namaz. you will learn how to offer prayers and say duas ",
+  },
+];
+export default courseArray;
