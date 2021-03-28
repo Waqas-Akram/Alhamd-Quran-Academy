d95b256870a9b4441e685d0dd45d9c834ce61610
Waqas Akram <waqasassasin@gmail.com>
2021-03-28 14:47:43 +0500

    Features array

---
+++ frontend/src/components/layout/FeatureArray.js
@@ -0,0 +1,22 @@
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import OfflineBoltIcon from '@material-ui/icons/OfflineBolt';
+const featureArray = [
+  {
+    id: 1,
+    title: "24/7 Availability",
+    icon: <AccessTimeIcon style={{fontSize:"70px"}} className="text-success"/>,
+  },
+  {
+    id: 2,
+    title: "3 days free trial",
+    icon: <OfflineBoltIcon style={{fontSize:"70px"}} className="text-info"/>,
+  },
+  {
+    id: 3,
+    title: "Access any where ",
+    icon: <LocationOnIcon style={{fontSize:"70px"}} className="text-primary"/>,
+  },
+];
+
+export default featureArray;
