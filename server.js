48544ca5a287007a188c30a6a2e6eeac4a2567c2
Waqas Akram <waqasassasin@gmail.com>
2021-03-28 18:15:43 +0500

    Ready for deployment

--- server.js
+++ server.js
@@ -20,10 +20,18 @@ app.use("/api/students", students);
 app.use("/api/contact", contact);
 app.use("/api/sendmessage", sendmessage);
 
-//Run api server
-app.get("/", (req, res) => {
-  res.json({ msg: "Api is Running" });
-});
+//Run api server and frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
